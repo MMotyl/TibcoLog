83659ddf0a3c9aa8e8307e1c2012c3a8f5caa99d
marcin.motyl@outlook.com <marcin.motyl@outlook.com>
2017-02-06 22:40:58 +0100

    zabay konslowoe

--- TibcoLog/Program.cs
+++ TibcoLog/Program.cs
@@ -8,8 +8,45 @@ namespace TibcoLog
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            bool ok = true;
+            string inputFile = "";
+            string searchString = "";
+
+            Console.WriteLine("Tibco log analyzer v.{0}", typeof(Program).Assembly.GetName().Version);
+            
+            foreach (String par in args)
+                {
+                    if (par.StartsWith("-f", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        try
+                        {
+                            inputFile = par.Substring(3);
+                        }
+                        catch { } 
+                    }
+                    if (par.StartsWith("-s", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        try
+                        {
+                            searchString = par.Substring(3);
+                        }
+                        catch { }
+                    }
+
+                }
+
+            if ((args.Length < 2) ||(inputFile.Length*searchString.Length==0))
+            {
+                ok = false;
+                Console.WriteLine("[E] niepoprawna liczba lub błędne wartości parametrów");
+                Console.WriteLine("-f:nazwa pliku do przeszukania");
+                Console.WriteLine("-s:szukana fraza [nie regexp]");
+            }
+            
         }
+        
     }
 }
---
+++ TibcoLog/jobs.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TibcoLog
+{
+    /// <summary>
+    /// basic element - single chunk of job instance
+    /// </summary>
+    class jobPart
+    {
+        public int lineFrom;
+        public int lineTo;
+        public bool lineHit;
+    }
+
+    /// <summary>
+    /// element - collection of chunks
+    /// </summary>
+    class job
+    {
+        int jobID;
+        int numberOfParts;
+        int lastChunkIndex;
+        int maxCapacity = 100; // na razie zakładam taki maksymalny rozmiar tablicy
+        private jobPart[] list; 
+        public job(int ID, int fromLine, int toLine, bool hit=false)
+        {
+            list =  new jobPart[maxCapacity];
+            jobID = ID;
+            lastChunkIndex = 0;
+            addChunk(fromLine, toLine, hit);
+        }
+
+        public void addChunk(int fromLine, int toLine, bool isHit=false)
+        {
+            if (lastChunkIndex < maxCapacity)
+            {
+                list[lastChunkIndex].lineFrom = fromLine;
+                list[lastChunkIndex].lineTo = toLine;
+                list[lastChunkIndex].lineHit = isHit;
+                lastChunkIndex++;
+                Console.WriteLine("Job {0} inc length to {1}", jobID, list.Length);// ewentualnie lastChunkIndex
+            }
+            else
+            {
+                Console.WriteLine("Job {0} has reached it max capacity", jobID);
+              
+            }
+        }
+
+        public IEnumerable<jobPart> GetChunk()
+        {
+            int index = 0;
+            do
+            {
+                if ((index < 0)||(index > lastChunkIndex))
+                    yield break;
+                yield return list[index];
+                index ++;
+            } while (true);
+        }
+
+    }
+
+    class LogParser
+    {
+        List<job> jobArray;
+        public LogParser()
+        {
+            jobArray = new List<job>();
+        }
+
+    }
+}
